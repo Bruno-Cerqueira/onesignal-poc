9e42d950aa6fc5e27223748aa1c0fa03f578d2f4
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-05-13 12:41:51 -0300

    Add more configurations and adjust button animation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,47 @@ export class AppComponent {
   constructor(private oneSignal: OneSignal) {
     this.oneSignal.init({
       appId: "5ba43a68-51f0-4de8-983a-144873c6ef55",
+      promptOptions: {
+        actionMessage: 'null',
+        exampleNotificationTitleDesktop: 'null',
+        exampleNotificationMessageDesktop:'null',
+        exampleNotificationTitleMobile:'null',
+        acceptButtonText:'null',
+        cancelButtonText:'null',
+        showCredit: false,
+        exampleNotificationCaption: 'null'
+      }
+    }).then(data => {
+
+      this.oneSignal.isPushNotificationsEnabled().then(isEnabled => {
+        console.log(isEnabled)
+        this.oneSignal.getSubscription().then(d => {
+          console.log(d)
+        });
+        this.oneSignal.getUserId().then(userId =>{
+          console.log(userId)
+          if(!isEnabled && !!userId) {
+            this.oneSignal.registerForPushNotifications({modalPrompt: true}).then(d =>{console.log(d)})
+          } 
+        })
+      })
+      /* this.oneSignal.isPushNotificationsEnabled().then(data => {
+        console.log(data)
+        this.oneSignal.getUserId().then(data =>{
+          console.log(data)
+          if(!data) {
+            
+          }
+        })
+      }) */
+    })
+
+    this.oneSignal.on('subscriptionChange', () => {
+      console.log('change')
+    })
+
+    this.oneSignal.on('notificationDisplay', () => {
+      console.log('OneSignal notification displayed:');
     });
   }
 
@@ -37,5 +78,16 @@ export class AppComponent {
     this.bool = !this.bool;
     console.log("mutado: ", this.bool);
     this.oneSignal.setSubscription(this.bool);
+  }
+
+  subscribe(){
+    this.oneSignal.showSlidedownPrompt({
+      force: true,
+    })
+    
+  }
+
+  getTags() {
+    this.oneSignal.getTags().then(data => console.log(data))
   }
 }
